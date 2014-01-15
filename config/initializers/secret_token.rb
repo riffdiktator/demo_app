62a109a5ecb0598ed5ab8841aad1ffd40a4a9664
Tomasz Lipiński <developer@riffdiktator.com>
2014-01-15 22:23:02 +0100

    Startowy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '1b4611fb47f4ac2cb59444f4adb74020d8ba9c92493c906df251e1a98f897230ca8e371b6a70e83a05a3a8182757769b1a3c3cc675e47aa493e6b1186b8dd23b'
